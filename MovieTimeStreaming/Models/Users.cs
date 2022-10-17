26ad5968b7ce3e0853554ea5a103a531c31fc068
mohamadabdelhady <mohamadhady010@gmail.com>
2022-10-17 00:02:40 +0000

    created the home page and styled it

---
+++ MovieTimeStreaming/Models/Users.cs
@@ -0,0 +1,10 @@
+namespace MovieTimeStreaming.Models;
+
+public class Users
+{
+    public int id { get; set; }
+    public string email { get; set; }
+    public string profile_img { get; set; }
+    public string password { get; set; }
+    
+}
