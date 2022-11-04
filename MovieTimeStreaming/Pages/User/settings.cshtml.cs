da35ad9e7e32409c794cf1f601db2a24988c0be8
mohamadabdelhady <mohamadhady010@gmail.com>
2022-11-04 18:44:02 +0000

    added vueJs and started the settings page

--- MovieTimeStreaming/Pages/User/settings.cshtml.cs
+++ MovieTimeStreaming/Pages/User/settings.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,21 @@ namespace MovieTimeStreaming.Pages.User
 {
     public class settingsModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public string userImg { get; set; }
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+        public settingsModel(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+        public async Task OnGet()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            userImg = user.ProfileImage;
+            userName = user.UserName;
+            userEmail = user.Email;
         }
     }
 }
